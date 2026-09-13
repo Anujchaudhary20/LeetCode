05cd9473ed6240b51aa9f9289829556fdbbc2ae3
Anuj Singh <87393631+Anujchaudhary20@users.noreply.github.com>
2026-09-13 13:44:46 +0530

    Time: 3 ms (17.77%), Space: 44.7 MB (99.99%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int k = 0;
-
-        for(int num : nums){
-
-            if(num != 0){
-                nums[k++] = num;
-            }
+       //two pointer Approach 
+      
+       int left = 0;
+        for(int right =0;right<nums.length;right++){
+           if(nums[right]!=0){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+           }
         }
-
-        while(k < nums.length){
-            nums[k++] = 0;
-        }
-        }
+        System.gc();
+    }
 }
